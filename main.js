511e5fd91b243789f2f165c1cfa76322df803f6a
Carlos Perez <carlosperez@Carloss-MacBook-Pro.local>
2022-04-24 19:38:02 -0500

    worked on stuff

--- main.js
+++ main.js
@@ -2,42 +2,29 @@
 let deckid;
 let score = 0;
 
-//Getting a new deck and retrieving its id
-fetch('https://deckofcardsapi.com/api/deck/new/')
+//Getting a new shuffled deck and retrieving its id
+fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     .then(res => res.json())
     .then(data => {
         console.log(data);
         deckid = data.deck_id;
     })
 
-//New game function
+
+//NEW GAME EVENT LISTENER
 document.querySelector('.newGame').addEventListener('click', function(){
     reset();
-    fetch(`https://deckofcardsapi.com/api/deck/${deckid}/draw/?count=1`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            
-            let card = document.createElement('img');
-            card.src = data.cards[0].image;
-            document.querySelector('.cards').appendChild(card);
-        })
+    drawCard();
+    console.log(score);
+    
 });
 
-//Draw 1 card function
+//HIT ME EVENT LISTENER
 document.querySelector('.hitMe').addEventListener('click', function() {
-    fetch(`https://deckofcardsapi.com/api/deck/${deckid}/draw/?count=1`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-
-            let card = document.createElement('img');
-            card.src = data.cards[0].image;
-            document.querySelector('.cards').appendChild(card);
-        })
+    drawCard();
 })
 
-//Stop drawing and check win condition
+//STOP EVENT LISTENER
 document.querySelector('.stop').addEventListener('click', function(){
 
 })
@@ -48,4 +35,33 @@ function reset() {
         cards.removeChild(cards.firstChild);
     }
     score = 0;
+}
+
+function drawCard() {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckid}/draw/?count=1`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+
+        //place image on the screen
+        let card = document.createElement('img');
+        card.src = data.cards[0].image;
+        document.querySelector('.cards').appendChild(card);
+
+        //log the card value to 'score'
+        let cardValue = data.cards[0].value;
+        if(cardValue === "JACK" || cardValue === "QUEEN" || cardValue === "KING") {
+            cardValue = 10;
+        }else if(cardValue === "ACE") {
+            let input = Number(prompt("You drew an ace. 1 or 11?"));
+            while(![1, 11].includes(input)) {
+                input = Number(prompt("Try again. 1 or 11?"));
+            }
+            cardValue = input;
+        }
+        score += Number(cardValue);
+
+        //display score to the screen
+        document.querySelector('.score').innerText = score;
+    })
 }
